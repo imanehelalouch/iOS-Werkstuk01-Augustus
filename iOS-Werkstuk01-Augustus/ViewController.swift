426b79d228933ce8a8064a811b64934c24bf5029
Imane Helalouch <imane.helalouch@student.ehb.be>
2019-08-12 15:51:40 +0200

    Initial Commit

---
+++ iOS-Werkstuk01-Augustus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Werkstuk01-Augustus
+//
+//  Created by Mohamed Helalouch on 12/08/2019.
+//  Copyright © 2019 Imane Helalouch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
